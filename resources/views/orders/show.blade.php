4d58af8e60a608d3433eebc6a7ce609c01829d1f
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-02-21 16:19:18 +0500

    comment fixed

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -89,7 +89,7 @@
                     @csrf
                     <div class="mb-3">
                         <label for="content" class="form-label">Your Comment</label>
-                        <textarea name="content" id="content" class="form-control" rows="3" required></textarea>
+                        <textarea name="comment_text" id="content" class="form-control" rows="3" required></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit Comment</button>
                 </form>
